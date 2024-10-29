db51f435067d2da4b04ca0c81fd6101bc7851dbe
RuanVNBezerra <164013416+RuanVNBezerra@users.noreply.github.com>
2024-10-28 22:38:10 -0300

    add codigo

---
+++ Controllers/LivrosController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace ProjetoLivraria.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LivrosController : ControllerBase
+{
+    // fazendo uma função privada para retornar a lista de livros do JSON
+    private static List<Livro> livros = new List<Livro>()
+    {
+        new Livro{ id = 1, titulo = "O Senhor dos Aneis", autor = "J.R.R. Tolkien", editora = "WMF Martins Fontes", ano = 2010,
+         preco = 50.00, quantidade = 10},
+        new Livro{ id = 2, titulo = "Harry Potter e a pedra filosofal", autor = "J.K. Rowling", editora = "Rocco", ano = 2000, quantidade = 5,
+         preco = 40.00},
+    };
+    //criando um crud para a classe Livro
+    [HttpGet]
+    [Route("livros")]
+    //criando um metodo para retornar a lista de livros
+    // usando IEnumerable para retornar uma lista de livros e ActionResult para retornar um status code.
+    public ActionResult<IEnumerable<Livro>> PegarLivros()
+    {
+        return Ok(livros);
+    }
+    /*
+     1.	Anotação [HttpGet("{id}")]:
+•	Esta anotação indica que o método responde a requisições HTTP GET no formato api/livros/{id}, 
+    onde {id} é um parâmetro que será passado na URL. 
+    Por exemplo, uma requisição para api/livros/1 chamará este método com id igual a 1.
+     */
+    [HttpGet("{id}")]
+    /*
+    O método é definido como public, o que significa que pode ser acessado de fora da classe.
+	Retorna um ActionResult<Livro>, que é um tipo de retorno que pode representar diferentes tipos de respostas HTTP, incluindo sucesso (Ok) e erro (NotFound).
+	O parâmetro int id é o ID do livro que se deseja obter.
+     */
+    public ActionResult<Livro> PegarLivro(int id)
+    {   /*
+        var livro = livros.FirstOrDefault(x => x.id == id);
+	    Esta linha usa LINQ para procurar o primeiro livro na lista livros que tenha o id igual ao valor passado como parâmetro.
+        Se nenhum livro for encontrado, FirstOrDefault retorna null.
+         */
+        var livro = livros.FirstOrDefault(x => x.id == id);
+        if (livro == null)
+        {
+            return NotFound();
+        }
+        return Ok(livro);
+    }
+
+    // criando um metodo para criar um livro
+    /*
+     	[HttpPost]: Indica que este método responde a requisições HTTP POST. Isso é usado para criar novos recursos no servidor.
+     */
+    [HttpPost]
+
+    /*
+     	public ActionResult<Livro> CriarLivro(Livro livro): Define um método público que retorna um ActionResult<Livro>. 
+        O parâmetro livro é um objeto do tipo Livro que será enviado no corpo da requisição. 
+     */
+    public ActionResult <Livro> CriarLivro(Livro livro)
+    {
+        /*
+         1.	livro.id = livros.Max(numeroId => numeroId.id);
+	Objetivo: Atribuir um ID único ao novo livro.
+	Detalhes:
+	livro.id = livros.Max(numeroId => numeroId.id) + 1; para garantir que o novo livro tenha um ID único.
+         */
+        livro.id = livros.Max(numeroId => numeroId.id) + 1;
+        /*
+         	Objetivo: Adicionar o novo livro à lista de livros.
+	        Detalhes: A lista livros é uma coleção estática que armazena todos os livros. Esta linha adiciona o novo livro a essa lista.
+         */
+        livros.Add(livro);
+        /*
+         	Objetivo: Retornar uma resposta HTTP 201 Created.
+	Detalhes:
+	CreatedAtAction(nameof(PegarLivro), new { id = livro.id }, livro): Cria uma resposta que inclui um cabeçalho Location apontando para o recurso recém-criado. O método PegarLivro será usado para gerar a URL do novo recurso.
+	new { id = livro.id }: Passa o ID do novo livro como parâmetro para a URL.
+	livro: O corpo da resposta incluirá o objeto livro recém-criado.
+         */
+        return CreatedAtAction(nameof(PegarLivro), new { id = livro.id }, livro);
+    }
+
+    // atualizar livro
+
+
+
+
+
+
+
+
+
+
+
+    // deletar livro
+}
---
+++ Livro.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoLivraria;
+// aqui ficará as variaveis e metodos da classe Livro
+public class Livro
+{
+    public int id { get; set; }
+    public string? titulo { get; set; }
+    public string? autor { get; set; }
+    public string? editora { get; set; }
+    public int ano { get; set; }
+    public double preco { get; set; }
+    public int quantidade { get; set; }
+}
